b51355d802431cc414b4bda2e436c89453b2f96b
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-06-27 14:26:11 +0200

    some changes

--- JavaScripts/Objects/main.js
+++ JavaScripts/Objects/main.js
@@ -1,38 +1,56 @@
-// Enhanced Object Literal
-// const person = {
-//   name: 'Ntwali',
-//   age: 25,
-//   hobbies: ['Sports', 'Cooking'],
-//   address: {
-//     city: 'Johannesburg',
-//     country: 'South Africa',
-//   },
-//   getBirthYear: function () {
-//     return 2021 - this.age;
+// // Enhanced Object Literal
+// // const person = {
+// //   name: 'Ntwali',
+// //   age: 25,
+// //   hobbies: ['Sports', 'Cooking'],
+// //   address: {
+// //     city: 'Johannesburg',
+// //     country: 'South Africa',
+// //   },
+// //   getBirthYear: function () {
+// //     return 2021 - this.age;
+// //   }
+// // }
+
+// const user = (name, age, password, work) => {
+//   return {
+//     name,
+//     age,
+//     password,
+//     work,
+//     getBirthYear() {
+//       return 2024 - this.age;
+//     }
 //   }
-// }
+// };
 
-const user = (name, age, password, work) => {
-  return {
-    name,
-    age,
-    password,
-    work,
-    getBirthYear() {
-      return 2024 - this.age;
-    }
-  }
+// const ntwali = user('Ntwali', 27, '@pass123', 'Enginner');
+// console.log(ntwali.getBirthYear());
+// console.log(ntwali);
+
+
+// const lib = {
+//   sum: (a, b) => a + b,
+//   mult: (a, b) => a * b
+// };
+
+// console.log(lib.sum(2, 3));
+// console.log(lib.mult(2, 3));
+
+//SPREAD OPERATOR used to make shallow copies of objects/
+function placeholder(a, b, c) {
+  console.log(a, b, c);
 };
 
-const ntwali = user('Ntwali', 27, '@pass123', 'Enginner');
-console.log(ntwali.getBirthYear());
-console.log(ntwali);
+const numbers = [1, 2, 3];
+placeholder(...numbers);
 
+const person = {
+  name: 'Gentil',
+  age: 25,
+  gender: 'male',
+}
+console.log(person);
 
-const lib = {
-  sum: (a, b) => a + b,
-  mult: (a, b) => a * b
-};
-
-console.log(lib.sum(2, 3));
-console.log(lib.mult(2, 3));
+const clonePerson = { ...person, work: 'Youtuber' }; // shallow copy of person object
+console.log(clonePerson)
