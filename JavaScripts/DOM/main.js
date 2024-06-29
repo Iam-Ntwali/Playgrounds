6634bcb5039345b92840c91240caac535f0a6740
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-06-29 18:46:38 +0200

    some changes

--- JavaScripts/DOM/main.js
+++ JavaScripts/DOM/main.js
@@ -1,1 +1,28 @@
 // DOM -> Document Object Model
+const h1 = document.querySelector('h1');
+h1.classList.add('h1');
+console.log(h1.classList.contains('h1'))
+console.log(h1);
+// Assuming there's only one '.container' element for simplicity
+const containers = document.getElementsByClassName('container');
+
+if (containers.length > 0) {
+  const firstContainer = containers[0]; // Get the first (and possibly only) container
+  firstContainer.addEventListener('click', () => {
+    console.log(firstContainer.children[1].children[0])
+    firstContainer.style.backgroundColor = 'Teal';
+    firstContainer.children[1].children[0].textContent = 'Clicked';
+  });
+} else {
+  console.log('No elements found with class "container".');
+}
+
+const link = document.querySelector('a');
+console.log(link)
+
+link.setAttribute('href', 'https://www.google.com');
+console.log(link.getAttribute('href'))
+
+
+
+
