7265609502afaf6a1370f3aa9c76baafed580fb7
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-06-29 19:41:11 +0200

    some changes

--- JavaScripts/DOM/main.js
+++ JavaScripts/DOM/main.js
@@ -23,6 +23,53 @@ console.log(link)
 link.setAttribute('href', 'https://www.google.com');
 console.log(link.getAttribute('href'))
 
+// CREATE ELEMENT
+const newDiv = document.createElement('div');
+newDiv.style.backgroundColor = 'indigo';
+newDiv.setAttribute('class', 'second - div')
+newDiv.classList.add('container');
+newDiv.style.marginTop = '2rem  ';
+document.body.appendChild(newDiv);
 
+const title = document.createElement('h1');
+title.textContent = '2nd Div';
+title.classList.add('title');
+newDiv.appendChild(title);
+
+const paragraph = document.createElement('p');
+paragraph.textContent = 'Lorem Ipsum lore dk texts et dolor lorem';
+
+title.insertAdjacentElement('afterend', paragraph);
+
+// APPEND AND PREPEND
+const newParagraph = document.createElement('span');
+newParagraph.textContent = 'New paragraph';
+newDiv.append(newParagraph);
+
+newParagraph.prepend(document.createElement('h2').textContent = 'New Title ');
+
+const createBtn = document.querySelector("#createDiv");
+
+
+createBtn.onclick = () => {
+  const newDiv = document.createElement('div');
+  newDiv.style.backgroundColor = 'indigo';
+  newDiv.setAttribute('class', 'second - div')
+  newDiv.classList.add('container');
+  newDiv.style.marginTop = '2rem  ';
+  document.body.appendChild(newDiv);
+
+  const title = document.createElement('h1');
+  title.textContent = '2nd Div';
+  title.classList.add('title');
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'Delete';
+  deleteBtn.addEventListener('click', () => {
+    newDiv.remove();
+  });
+  newDiv.appendChild(title);
+  newDiv.appendChild(deleteBtn);
+};
 
 
