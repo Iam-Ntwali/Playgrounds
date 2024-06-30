cdf694ac10d8a3a17b73324c87cf2f465290f1cc
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-06-30 14:33:29 +0200

    some changes

--- JavaScripts/7. GSAP Basics/C. from&fromTo/From&fromTo.js
+++ JavaScripts/7. GSAP Basics/C. from&fromTo/From&fromTo.js
@@ -1,21 +1,38 @@
 // from: In from method you don't have to specifiy the initial state (It will figure out by itself)
 gsap.from('.box1', {
-  duration: 2,
-  y: -50,
+  duration: 1,
+  y: -20,
   ease: 'linear',
   repeat: -1,
   yoyo: true,
 });
+gsap.from('.box2', {
+  duration: 1,
+  y: 20,
+  ease: 'linear',
+  repeat: -1,
+  yoyo: true,
+});
+
+gsap.from('.box3', {
+  duration: 1,
+  y: -20,
+  ease: 'linear',
+  repeat: -1,
+  yoyo: true,
+});
+
+
 // fromTo: in this method you have to specifiy the default svalue, and also the new animation state.
-gsap.fromTo('.box2', {
-  y: 100,
-  opacity: 0,
-}, {
-  opacity: 1,
-  y: -100,
-  duration: 3,
-  ease: 'linear',
-  repeat: -1,
-  yoyo: true,
-  borderRadius: 0,
-});
+// gsap.fromTo('.box2', {
+//   y: 100,
+//   opacity: 0,
+// }, {
+//   opacity: 1,
+//   y: -100,
+//   duration: 3,
+//   ease: 'linear',
+//   repeat: -1,
+//   yoyo: true,
+//   borderRadius: 0,
+// });
