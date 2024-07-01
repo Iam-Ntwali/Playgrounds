c1ce86d8d06a81cd82f22e62bc144a450551197e
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-07-01 12:29:07 +0200

    some changes

--- JavaScripts/8. OOP/main.js
+++ JavaScripts/8. OOP/main.js
@@ -1,33 +1,49 @@
-console.log('Object Orientation Programming');
+// console.log('Object Orientation Programming');
 
 
-// this -> keyword refer always to the window object unless it is in an object instance
+// // this -> keyword refer always to the window object unless it is in an object instance
 
-console.log(this) // window object
+// console.log(this) // window object
 
-function printThis(name, age) {
-  return this;
-}
+// function printThis(name, age) {
+//   return this;
+// }
 
-console.log(printThis()); // still refering to the window object
+// console.log(printThis()); // still refering to the window object
 
-const user = {
-  name: 'Ntwali',
-  age: 25,
-  printThis() {
-    return this;
-  }
-}
+// const user = {
+//   name: 'Ntwali',
+//   age: 25,
+//   printThis() {
+//     return this;
+//   }
+// }
 
-console.log(user.printThis()); // now it refers to the user object
+// console.log(user.printThis()); // now it refers to the user object
 
-// The catch =>
-const user2 = {
-  name: 'John',
-  age: 30,
-  printThis: () => {
-    return this;
-  }
-}
+// // The catch =>
+// const user2 = {
+//   name: 'John',
+//   age: 30,
+//   printThis: () => {
+//     return this;
+//   }
+// }
 
-console.log(user2.printThis()); // still refers to the window object because arrow function does not have its own 'this'
+// console.log(user2.printThis()); // still refers to the window object because arrow function does not have its own 'this'
+
+// // Factory function => return objects
+// function createUser(name, age) {
+//   return {
+//     name,
+//     age,
+//     printThis() {
+//       return `My name is ${name} and I am ${age} years old.`;
+//     }
+//   }
+// }
+
+// const user3 = createUser('Jane', 28);
+// const user4 = createUser('Smith', 45);
+// console.log(user3.printThis()); // now it refers to the user3 object
+// console.log(user4.printThis()); // still refers to the user4 object
