65a04e38a125c64fd653b258cc81486e2bc44f7f
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-07-01 15:26:51 +0200

    some changes

--- JavaScripts/8. OOP/main.js
+++ JavaScripts/8. OOP/main.js
@@ -47,3 +47,15 @@
 // const user4 = createUser('Smith', 45);
 // console.log(user3.printThis()); // now it refers to the user3 object
 // console.log(user4.printThis()); // still refers to the user4 object
+
+// constructor function => act as blueprints for creating multiple instance of objects with the same structure and behaviours.
+
+function Course(name, description, sections) {
+  this.name = name;
+  this.description = description;
+  this.sections = sections;
+}
+
+const JavaScriotsBasics = new Course('JavaScript', 'This course will teach the best Job', 'Intro to JS Programming');
+
+console.log(JavaScriotsBasics.name);
