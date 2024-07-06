ad6b1a6b076d60b4b35b31b7732dbd7c278a13d5
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-07-06 16:54:07 +0200

    some changes

--- JavaScript/Dave-NodeJs/Server/server.js
+++ JavaScript/Dave-NodeJs/Server/server.js
@@ -6,37 +6,26 @@ const fsPromises = require('fs').promises;
 const logEvents = require('./logEvents');
 const EventEmitter = require('events');
 class Emitter extends EventEmitter { };
-// initialize object 
-const myEmitter = new Emitter();
-myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
-const PORT = process.env.PORT || 3500;
 
-const serveFile = async (filePath, contentType, response) => {
-    try {
-        const rawData = await fsPromises.readFile(
-            filePath,
-            !contentType.includes('image') ? 'utf8' : ''
-        );
-        const data = contentType === 'application/json'
-            ? JSON.parse(rawData) : rawData;
-        response.writeHead(
-            filePath.includes('404.html') ? 404 : 200,
-            { 'Content-Type': contentType }
-        );
-        response.end(
-            contentType === 'application/json' ? JSON.stringify(data) : data
-        );
-    } catch (err) {
-        console.log(err);
-        myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt');
-        response.statusCode = 500;
-        response.end();
-    }
-}
+// initialize object
+const myEmitter = new Emitter();
+
+// Port
+const PORT = process.env.PORT || 4500;
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
-    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
+
+    // let filePath;
+
+    // if (req.url === '/' || req.url === 'index.html') {
+    //     res.statusCode = 200;
+    //     res.setHeader('Content-Type', 'text/html');
+    //     filePath = path.join(__dirname, 'index.html');
+    //     fs.readFile(filePath, 'utf8', (err, data) => {
+    //         res.end(data);
+    //     });
+    // }
 
     const extension = path.extname(req.url);
 
@@ -47,53 +36,35 @@ const server = http.createServer((req, res) => {
             contentType = 'text/css';
             break;
         case '.js':
-            contentType = 'text/javascript';
+            contentType = 'application/javascript';
             break;
         case '.json':
             contentType = 'application/json';
             break;
-        case '.jpg':
+        case 'jpg':
             contentType = 'image/jpeg';
             break;
-        case '.png':
+        case 'png':
             contentType = 'image/png';
             break;
-        case '.txt':
+        case 'txt':
             contentType = 'text/plain';
             break;
         default:
             contentType = 'text/html';
+            break;
     }
 
-    let filePath =
-        contentType === 'text/html' && req.url === '/'
-            ? path.join(__dirname, 'views', 'index.html')
-            : contentType === 'text/html' && req.url.slice(-1) === '/'
-                ? path.join(__dirname, 'views', req.url, 'index.html')
-                : contentType === 'text/html'
-                    ? path.join(__dirname, 'views', req.url)
-                    : path.join(__dirname, req.url);
+})
 
-    // makes .html extension not required in the browser
-    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
-
-    const fileExists = fs.existsSync(filePath);
-
-    if (fileExists) {
-        serveFile(filePath, contentType, res);
-    } else {
-        switch (path.parse(filePath).base) {
-            case 'old-page.html':
-                res.writeHead(301, { 'Location': '/new-page.html' });
-                res.end();
-                break;
-            case 'www-page.html':
-                res.writeHead(301, { 'Location': '/' });
-                res.end();
-                break;
-            default:
-                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
-        }
-    }
+// Listen on port
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// // event listener for the log event
+// myEmitter.on('log', (msg) => logEvents(msg));
+
+// // Emit event
+// myEmitter.emit('log', 'Log event emitted');
+
