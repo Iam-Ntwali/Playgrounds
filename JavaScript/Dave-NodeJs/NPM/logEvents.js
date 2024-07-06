b1cfcf4b9b5916ca9cb88d0124ac77abcfb5183c
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-07-06 14:29:48 +0200

    some changes

--- JavaScript/Dave-NodeJs/NPM/logEvents.js
+++ JavaScript/Dave-NodeJs/NPM/logEvents.js
@@ -6,11 +6,14 @@ const fsPromises = require('fs').promises;
 const path = require('path');
 
 const logEvents = async (message) => {
-  const dateTime = `${format(new Date(), 'yyyy-MM-dd\tHH:mm:sss')}`;
-  const logItem = `${dateTime} \t${uuid()}\t${message}`;
+  const dateTime = `${format(new Date(), 'yyyy-MM-dd\tHH:mm:ss')}`;
+  const logItem = `${dateTime} \t${uuid()}\t${message}\n`;
   console.log(logItem);
 
   try {
+    if (!fs.existsSync(path.join(__dirname, 'logs'))) {
+      await fsPromises.mkdir(path.join(__dirname, 'logs'));
+    }
     await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), logItem);
 
   } catch (err) {
